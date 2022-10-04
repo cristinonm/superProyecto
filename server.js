89b6d0a1ee08c9047968aa24c14acd5e570e3154
cristinonm <cristino_navarro@hotmail.com>
2022-10-04 11:39:27 -0500

    Configuraciones inciales

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import stripe from 'stripe'
+import{ initializeApp } from 'firebase/app'
+
+//Configuracion de Firebase
+const firebaseConfig ={
+    
+}
